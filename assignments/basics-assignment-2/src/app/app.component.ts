b504a3f44ae2104d0ad4546677a787f0a0bdba6e
Md Rafi Akhtar <alimdrafi@gmail.com>
2023-11-05 16:58:04 +0530

    added solutions to first two assignments; not sure if they'd work, though

---
+++ assignments/basics-assignment-2/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  username = "";
+
+  resetUsername() {
+    if (this.username.length) {
+      return;
+    }
+    this.username = "";
+  }
+}
