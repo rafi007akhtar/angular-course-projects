633492a2d04bf05f2e5bceb54dc15f18483e7052
Md Rafi Akhtar <alimdrafi@gmail.com>
2023-11-18 16:10:58 +0530

    using the store to read and show the counter on UI; buttons don't work yet

--- 15-ngrx-without-standalone/src/app/store/counter.reducer.ts
+++ 15-ngrx-without-standalone/src/app/store/counter.reducer.ts
@@ -1,6 +1,10 @@
-import { ActionReducer, createReducer } from '@ngrx/store';
-import { Action } from 'rxjs/internal/scheduler/Action';
+import { createReducer } from '@ngrx/store';
 
 const initialState = 0;
 
 export const counterReducer = createReducer(initialState);
+
+// Alternative way of creating reducers in older NgRx / Angular versions
+// export const counterReducer = function (state = initialState) {
+//   return state;
+// };
